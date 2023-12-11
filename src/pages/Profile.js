4d4bc016420415364c719039a358adf01ca08ff2
S-123-satya <satyaprakash5056742@gmail.com>
2023-12-11 22:11:57 +0530

    login bug fix

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="mt-14 grid place-items-end">
+      <form>
+        <div className="flex justify-between m-2">
+          <h2 className="text-2xl  font-bold">Contact details</h2>
+          <button type="button" className="rounded border-2 px-1 font-bold border-red-300 text-red-600">Cancel</button>
+        </div>
+        <label className="ms-10" htmlFor="username">
+          Full name:{" "}
+        </label>
+        <input className="border-2 rounded mx-8" type="text" />
+        <label htmlFor="picUrl">Profile Photo Pic: </label>
+        <input className="border-2 rounded mx-8" type="text" />
+      </form>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
